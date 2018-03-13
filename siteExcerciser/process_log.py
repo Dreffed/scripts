cb243e23bcc3bf8306122e845953dc11691c77e2
David Gloyn-Cox <github@dreffed.com>
2018-03-13 15:06:13 -0700

    added export to csv

--- siteExcerciser/process_log.py
+++ siteExcerciser/process_log.py
@@ -2,6 +2,7 @@ import gzip
 import re
 import os
 import datetime
+import csv
 
 def addFiles(rootDir, regexFilter = None):
     filesToProcess = []
@@ -40,60 +41,65 @@ def process_file(file_data):
         return file_data
 
     re_date = re.compile(b'^([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}) - DEBUG -')
-    re_request = re.compile(b'response_time="(.*)" endpoint="(.*)" request="(.*)"')
-    re_response = re.compile(b'response="(.*)"')
-    re_uid = re.compile(b'uid="(.*)" request_uri="(.*)" referer="(.*)" host="(.*)" agent="(.*)"')
+    re_request = re.compile(r' response_time="(.*)" endpoint="(.*)" request="(.*)"')
+    re_response = re.compile(r' response="(.*)"')
+    re_uid = re.compile(b'.* uid="([0-9]+)"')
+    re_misc=re.compile(r' request_uri="(.*)" referer="(.*)" host="(.*)"')
+    re_agent=re.compile(b'.* agent="(.*)"')
     #response_time="(.*)" endpoint="(.*)" request="(.*)" response="(.*)" 
     #uid="([\d]+)" request_uri="(.*)" referer="(.*)" host="(.*)" agent="(.*)"
 
     i = 0
+    idx = 0
     file_name = file_data['path']
     rows = []
     row = {}
 
-    with gzip.open(file_name) as f:
+    with gzip.open(file_name, 'r') as f:
         for line in f:
             i += 1
+
             m = re.match(re_date, line)
-            print(m)
             if m:
                 if row:
-                    print(row)
                     rows.append(row)
-                row = {}       
+
+                row = {}
+                idx += 1
+                row['idx'] = idx
+                row['line'] = i
                 row['date'] = m.group(1)
-            
-            m = re.match(re_request, line)
-            print(m)
-            if m:
-                row['response_time'] = m.group(1)
-                row['endpoint'] = m.group(2)
 
-            m = re.match(re_uid, line)
-            print(m)
-            if m:
-                row['uid'] = m.group(1)
-                row['referer'] = m.group(3)
-                row['host'] = m.group(4)
-                row['agent'] = m.group(5)
+            mu = re.match(re_uid, line)
+            if mu:
+                row['uid'] = mu.group(1)
 
-    if row:
-        rows.append(row)
+            ma = re.match(re_agent, line)
+            if ma:
+                row['agent'] = ma.group(1)
 
-    print(i)
+        if row:
+            rows.append(row)
+    
     file_data['lines'] = len(rows)
+      
     return file_data, rows
+
+def save_rows(file_name, rows):
+    f_path = '{}.csv'.format(file_name)
+    keys = ['uid', 'agent', 'date', 'idx', 'line']
+
+    with open(f_path, 'w', newline='') as f:
+        writer = csv.DictWriter(f, keys)    
+        writer.writeheader()
+        writer.writerows(rows)
 
 def main():
     file_path = r"C:\opt\S3Bucket"
     file_list = select_files(file_path)
     for a_file in file_list:
         a_file, rows = process_file(a_file)
-        print('\t{} => {}'.format(a_file['lines'], a_file['path']))
-        if rows:
-            print(rows[0])
-            print(rows[-1])
-        break
+        save_rows(a_file['name'], rows)
 
 if __name__ == "__main__":
     main()
